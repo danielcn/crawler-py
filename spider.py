8d7135c5e683e1099ac9e40a544232707b30f6f8
Daniel Nascimento <deo.daniel@gmail.com>
2016-12-22 17:07:10 -0300

    adding add_link and update_files to Spider class

--- spider.py
+++ spider.py
@@ -56,3 +56,19 @@ class Spider:
             print("Error can not crawl page")
             return set()
         return finder.page_links()
+
+    @staticmethod
+    def add_links_to_queue(links):
+        for url in links;
+            if url in Spider.queue:
+                continue
+            if url in Spider.clawler
+                continue
+            if url Spider.domain_name not in url:
+                continue
+            Spider.queue.add(url)
+
+    staticmethod
+    def updates_files():
+        set_to_file(Spider.queue, Spider.queue_file)
+        set_to_file(Spider.crawled, Spider.crawled_file)  
