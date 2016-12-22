c117191cc2b4dfae0eb70f593d949e5c8751daee
Daniel Nascimento <deo.daniel@gmail.com>
2016-12-22 10:19:08 -0300

    adding append and delete file functions

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def create_data_files(project_name, base_url):
     if not os.path.isfile(queue):
         write_file(queue, base_url)
     if not os.path.isfile(crawled):
-        write_file(crawled, 'cd .')
+        write_file(crawled, '')
 
 # Create a new file
 def write_file(path, data):
@@ -21,4 +21,12 @@ def write_file(path, data):
     f.write(data)
     f.close()
 
-create_data_files('thenewboston', 'http://thenewboston.com/')
+# Add data onto an existing files
+def  append_to_file(path, data):
+    with open(path, 'a') as file:
+        file.write(data + '\n')
+
+# Delete the contetns of a file
+def delete_file_contets(path):
+    with open(path, 'w'):
+        pass
