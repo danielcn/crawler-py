1bc3fca2d91ee2de2e45e8275111f380cf054ad8
Daniel Nascimento <danascimen@thoughtworks.com>
2019-08-08 07:00:23 -0300

    Fixing imports

--- link_finder.py
+++ link_finder.py
@@ -1,22 +1,23 @@
 from html.parser import HTMLParser
+from urllib.parse import urljoin
 
 class LinkFinder(HTMLParser):
 
     def _init_(self, base_url, page_url):
-        super(self)._init_()
+        super().__init__()
         self.base_url = base_url
         self.page_url = page_url
         self.links = set()
 
-    def handle_starttag(self, tag, atts):
+    def handle_starttag(self, tag, attrs):
         if tag == 'a':
             for(attribute, value) in attrs:
                 if attribute == 'href':
-                    url = parse.urljoin(self.base_url, value)
+                    url = urljoin(self.base_url, value)
                     self.links.add(url)
 
-    def page_links(self):
-        return self.links
+    def handle_endtag(self, tag):
+        pass
 
     def error(self, message):
         pass
